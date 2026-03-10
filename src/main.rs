9877a9014dfbb6df51bf6a6e9caa6a7771604efa
Victor Lellis <vmlellis@gmail.com>
2026-03-10 15:48:07 -0300

    feat: rendering & multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::error::Error;
-use std::io;
+use std::sync::mpsc;
+use std::{io, thread};
 use std::time::Duration;
 use crossterm::cursor::{Hide, Show};
 use crossterm::event::{Event, KeyCode};
 use crossterm::{ExecutableCommand, event, terminal};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use invaders::frame::new_frame;
+use invaders::{frame, render};
 use rusty_audio::Audio;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -24,8 +27,27 @@ fn main() -> Result<(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
+    // Render loop in a separate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame =match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &last_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
     // Game Loop
     'gameloop: loop {
+        // Per-frame init
+        let curr_frame = new_frame();
+
         // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
@@ -38,9 +60,15 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+
+        // Draw & render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
     }
 
     // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
