8ecc3af8d883e47b27eaa8d7b8edf6d55744fe1a
Victor Lellis <vmlellis@gmail.com>
2026-03-17 13:05:14 -0300

    feat: winning and losing

--- src/invaders.rs
+++ src/invaders.rs
@@ -77,11 +77,31 @@ impl Invaders {
 
         false
     }
+
+    pub fn all_killed(&self) -> bool {
+        self.army.is_empty()
+    }
+
+    pub fn reached_bottom(&self) -> bool {
+        self.army.iter().map(|invader| invader.y).max().unwrap_or(0) >= NUM_ROWS - 1
+    }
+
+    pub fn kill_invader_at(&mut self, x: usize, y: usize) -> bool {
+        if let Some(i) = self.army.iter().position(|invader| invader.x == x && invader.y == y) {
+            self.army.remove(i);
+            true
+        } else {
+            false
+        }
+    }
 }
 
 impl Drawable for Invaders {
     fn draw(&self, frame: &mut Frame) {
         for invader in self.army.iter() {
+            if invader.x >= frame.len() || invader.y >= frame[invader.x].len() {
+                continue;
+            }
             frame[invader.x][invader.y] = if (self.move_timer.remaining().as_secs_f32()
                 / self.move_timer.duration().as_secs_f32())
                 > 0.5
--- src/main.rs
+++ src/main.rs
@@ -80,6 +80,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         if invaders.update(delta) {
             audio.play("move");
         }
+        if player.detect_hits(&mut invaders) {
+            audio.play("explode");
+        }
 
         // Draw & render
         let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
@@ -88,6 +91,12 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
+
+        // Win or lose?
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'gameloop;
+        }
     }
 
     // Cleanup
